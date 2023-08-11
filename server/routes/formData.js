3a2c302eec4f9e3221f06685540f9665e3db838b
delahocheemmanuel <delahochemanu@hotmail.fr>
2023-08-11 17:08:28 +0200

    modif con4r

--- server/routes/formData.js
+++ server/routes/formData.js
@@ -2,11 +2,11 @@
 const express = require("express");
 const router = express.Router();
 const FormDataController = require("../controllers/formData");
-const limiter = require("../middleware/rate-limit"); // Middleware pour limiter le nombre de requêtes par IP
+
 
 
 
 // Route pour envoyer un e-mail
-router.post("/Contact", limiter, FormDataController.sendFormData);
+router.post("/contact", FormDataController.sendFormData);
 
 module.exports = router;
